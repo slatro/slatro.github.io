9d90187c8495428c4bcb0efe529ed26fd2852272
slatro <52999425+slatro@users.noreply.github.com>
2024-06-23 02:28:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 document.getElementById('selectButton').addEventListener('click', function() {
-    fetch('https://api.api-ninjas.com/v1/celebrity?nationality=tr', {
+    // Rastgele bir ofset değeri oluştur (0-99 arasında)
+    const randomOffset = Math.floor(Math.random() * 100);
+    fetch(`https://api.api-ninjas.com/v1/celebrity?nationality=tr&offset=${randomOffset}`, {
         headers: { 'X-Api-Key': 'X//XRcfxQiNykNdZo3E0UQ==amsPq0cijigHxwOe' }
     })
     .then(response => response.json())
     .then(data => {
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const celebrity = data[randomIndex];
-        document.getElementById('celebrityDisplay').innerText = celebrity.name;
+        if(data.length > 0) {
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const celebrity = data[randomIndex];
+            document.getElementById('celebrityDisplay').innerText = celebrity.name;
+        } else {
+            document.getElementById('celebrityDisplay').innerText = 'Ünlü bulunamadı';
+        }
     })
     .catch(error => console.error('Error:', error));
 });
