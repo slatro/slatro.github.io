bff69b7132c1575d6a583ac986ea7b060cd55a0a
slatro <52999425+slatro@users.noreply.github.com>
2024-06-23 02:32:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('selectButton').addEventListener('click', function() {
-    // Rastgele bir ofset değeri oluştur (0-99 arasında)
-    const randomOffset = Math.floor(Math.random() * 100);
+    const randomOffset = Math.floor(Math.random() * 50);
     fetch(`https://api.api-ninjas.com/v1/celebrity?nationality=tr&offset=${randomOffset}`, {
         headers: { 'X-Api-Key': 'X//XRcfxQiNykNdZo3E0UQ==amsPq0cijigHxwOe' }
     })
     .then(response => response.json())
     .then(data => {
+        console.log(data); // API'den dönen veriyi kontrol etmek için
         if(data.length > 0) {
             const randomIndex = Math.floor(Math.random() * data.length);
             const celebrity = data[randomIndex];
